1fd36fa2a5eaddd7f8af47247f81f50135e193bc
Syed Kashif Ali Ashraf Ali <kashifsayyad09@gmail.com>
2026-06-23 17:52:51 +0530

    Comment out S3 bucket resource in main.tf
    Comment out the aws_s3_bucket resource definition.

--- terraform-day-11-userdata-dynatrace/main.tf
+++ terraform-day-11-userdata-dynatrace/main.tf
@@ -54,6 +54,6 @@ resource "aws_security_group" "allow_ssh" {
 #i wanna import existing ec2 maunally created in aws console, how can i do that?
 # Run: terraform import aws_instance.existing i-05e3b01f6d60a08d3
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bktdsbjjgkkmdlsme-12345"
-}
+#resource "aws_s3_bucket" "my_bucket" {
+#  bucket = "my-unique-bktdsbjjgkkmdlsme-12345"
+#}
